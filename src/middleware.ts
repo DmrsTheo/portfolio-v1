364ea247dfa7336828cf803d748e527f2873052f
Théo De Morais <demoraistheo@gmail.com>
2026-03-02 23:37:38 +0100

    force deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+// test
 export function middleware(request: NextRequest) {
   
   const basicAuth = request.headers.get('authorization');
